ffaf480b774a43b016e051095ee4c9c142ddcebb
micaellesilvaa <micaelle.silva@liferay.com>
2023-12-15 14:09:48 -0300

    first commit

---
+++ primeiraAula/index1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Data em PHP</title>
+        <meta charset="utf-8">
+    </head>
+<body>
+    <?php
+    //Aqui segue seu código em PHP
+        echo "Vou aprender php";
+        echo "Sou um bom aluno";
+    ?>
+</body>
+</html>
